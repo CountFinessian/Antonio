a41b61df727aa6603bc974449063bdc231c74028
Jacob Williams <jawoba004@gmail.com>
2024-06-02 14:15:40 -0600

    Logs out a user.

--- server/src/main/java/dataaccess/SQLAuthDAO.java
+++ server/src/main/java/dataaccess/SQLAuthDAO.java
@@ -3,6 +3,7 @@ package dataaccess;
 import model.AuthData;
 import model.UserData;
 
+import java.sql.SQLException;
 import java.util.UUID;
 
 public class SQLAuthDAO implements AuthDAO{
@@ -23,13 +24,30 @@ public class SQLAuthDAO implements AuthDAO{
 
     @Override
     public void logoutAuth(String authToken) throws DataAccessException {
-
+        var statement = "DELETE FROM AuthData WHERE authToken=?";
+        SQLUserDAO.executeUpdate(statement, authToken);
     }
 
     @Override
     public AuthData getAuth(String authToken) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT authToken, username FROM AuthData WHERE authToken=?";
+            try (var ps = conn.prepareStatement(statement)) {
+                ps.setString(1, authToken);
+                try (var rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        String authToken2 = rs.getString("authToken");
+                        String username2 = rs.getString("username");
+                        return new AuthData(authToken2, username2);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Unable to read data: " + e.getMessage());
+        }
         return null;
     }
+
 
     @Override
     public void clearAuths() throws DataAccessException {
